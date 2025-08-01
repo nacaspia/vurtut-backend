83bf3c0d45916bc50903c104713fa22951a8d9a5
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-01 12:51:35 +0400

    bugfix

--- resources/views/site/company/home.blade.php
+++ resources/views/site/company/home.blade.php
@@ -379,8 +379,143 @@
     </div>
 @endsection
 @section('company.js')
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAAz77U5XQuEME6TpftaMdX0bBelQxXRlM&amp;callback=initMap"></script>
-    <script src="{{ asset('site/js/googlemaps1.js') }}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD8M9rUVW_Og-Z8sTfQSA5HUgRbd4WyW0w&amp;callback=initMap"></script>
+    <script>
+        var MY_MAPTYPE_ID = 'style_KINESB';
+
+        function initialize() {
+            var featureOpts = [
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#666666"
+                        }
+                    ]
+                },
+                {
+                    "featureType": 'all',
+                    "elementType": 'labels',
+                    "stylers": [
+                        { visibility: 'simplified' }
+                    ]
+                },
+                {
+                    "featureType": "landscape",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#e2e2e2"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "saturation": -100
+                        },
+                        {
+                            "lightness": 45
+                        },
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.arterial",
+                    "elementType": "labels.icon",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "transit",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "visibility": "off"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "elementType": "all",
+                    "stylers": [
+                        {
+                            "color": "#aadaff"
+                        },
+                        {
+                            "visibility": "on"
+                        }
+                    ]
+                }
+            ];
+            var myGent = new google.maps.LatLng({{$company['data']['lat'] ?? ''}},{{$company['data']['lng'] ?? ''}});
+            var Kine =  new google.maps.LatLng({{$company['data']['lat'] ?? ''}},{{$company['data']['lng'] ?? ''}});
+            var mapOptions = {
+                zoom: 11,
+                mapTypeControl: true,
+                zoomControl: true,
+                zoomControlOptions: {
+                    style: google.maps.ZoomControlStyle.SMALL,
+                    position: google.maps.ControlPosition.LEFT_TOP,
+                    mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+                },
+                mapTypeId: MY_MAPTYPE_ID,
+                scaleControl: false,
+                streetViewControl: false,
+                center: myGent
+            }
+            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+            var styledMapOptions = {
+                name: 'style_KINESB'
+            };
+
+            var image = {
+                url: '{{ asset('site/images/gps.png') }}',  // İkon şəkilin URL-i
+                size: new google.maps.Size(32, 32),               // Orijinal ölçü
+                origin: new google.maps.Point(0, 0),              // İkonun başlanğıc nöqtəsi
+                anchor: new google.maps.Point(16, 32),            // İkonun "göyərçin quyruğu" nöqtəsi (aşağı ortası)
+                scaledSize: new google.maps.Size(32, 32)          // Xəritədə görünəcək ölçü (ölçüləndir)
+            };
+            var marker = new google.maps.Marker({
+                position: Kine,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: 'B4318, Gumfreston SA70 8RA, United Kingdom',
+                icon: image
+            });
+
+            var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+            map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
     <script>
         $(document).ready(function () {
             $(document).on('click', '.reply-btn', function (e) {
