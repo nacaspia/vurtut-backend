92bf2267fe13874f6cf5bd34a6dd34fd6a2221e1
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-05 14:12:19 +0400

    bugfix

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -4,9 +4,17 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="airbnb, booking, city guide, directory, events, hotel booking, listings, marketing, places, restaurant, restaurant">
-    <meta name="description" content="Guido - Directory & Listing HTML Template">
-    <meta name="CreativeLayers" content="ATFN">
+    <meta name="keywords" content="Vurtut, xidmət platforması, restoran rezervasiyası, salon sifarişi, otel bronlama, biznes siyahısı, müştəri tapmaq, online rezervasiya, marketplace, Azərbaycan, yerli bizneslər">
+    <!-- Təsvir (SEO üçün) -->
+    <meta name="description" content="Vurtut – restoran, otel, salon və digər xidmət sahələrini bir araya gətirən onlayn platforma. Məkanını əlavə et, görünürlük qazan, müştəri tap. İlk ay ödənişsiz!">
+    <!-- Müəllif məlumatı -->
+    <meta name="author" content="vurtut.com">
+    <meta property="og:title" content="Vurtut - Hamısı bir ünvanda!">
+    <meta property="og:description" content="Restoran, salon, otel və digər məkanlar üçün onlayn rezervasiya, görünürlük və müştəri qazanmaq imkanı.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://vurtut.com">
+    <meta property="og:image" content="{{ asset("site/images/Vurtut logo icon/Vurtut.com.ico") }}">
+    <meta property="og:site_name" content="Vurtut">
     <title>vurtut.com</title>
     <link href="{{ asset("site/images/Vurtut logo icon/Vurtut.com.ico") }}" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
     <link href="{{ asset("site/images/Vurtut logo icon/Vurtut.com.ico") }}" sizes="128x128" rel="shortcut icon" />
