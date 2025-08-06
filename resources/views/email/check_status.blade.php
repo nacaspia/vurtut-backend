00579265a0e19f2f5c4ba12acc272edfdb7d4f82
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-06 22:33:39 +0400

    bugfix

--- resources/views/email/check_status.blade.php
+++ resources/views/email/check_status.blade.php
@@ -3,20 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content="airbnb, booking, city guide, directory, events, hotel booking, listings, marketing, places, restaurant, restaurant">
-    <meta name="description" content="Guido - Directory & Listing HTML Template">
-    <meta name="CreativeLayers" content="ATFN">
     <!-- css file -->
-    <link rel="stylesheet" href="https://vurtut.com/site/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://vurtut.com/site/css/style.css">
+    <link rel="stylesheet" href="{{ asset('site/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('site/css/style.css') }}">
     <!-- Responsive stylesheet -->
-    <link rel="stylesheet" href="https://vurtut.com/site/css/responsive.css">
+    <link rel="stylesheet" href="{{ asset('site/css/responsive.css') }}">
     <!-- Title -->
     <title>vurtut.com</title>
     <!-- Favicon -->
-    <link href="https://vurtut.com/site/images/Vurtut logo icon/Vurtut.com.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
-    <link href="https://vurtut.com/site/images/Vurtut logo icon/Vurtut.com.ico" sizes="128x128" rel="shortcut icon" />
+    <link href="{{ asset('site/images/Vurtut logo icon/Vurtut.com.ico') }}" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
+    <link href="{{ asset('site/images/Vurtut logo icon/Vurtut.com.ico') }}" sizes="128x128" rel="shortcut icon" />
 
 </head>
 <body>
@@ -31,78 +27,32 @@
                     <div class="invoice_table">
                         <div class="row mb25">
                             <div class="col-lg-7">
-                                <div class="main_logo fz30 color-black22 fw500"><img class="img-fluid" src="https://vurtut.com/site/images/Vurtut logo icon/vurtut.com.svg" alt="header-logo2.svg"> vurtut.com</div>
+                                <div class="main_logo fz30 color-black22 fw500"><img class="img-fluid" src="{{ asset('site/images/Vurtut logo icon/vurtut.com.svg') }}" alt="header-logo2.svg"> vurtut.com</div>
                             </div>
                             <div class="col-lg-5">
                                 <div class="invoice_deails">
-                                    <h3 class="float-left">Invoice #</h3>
-                                    <span class="float-right">0043128641</span>
+                                    <h3 class="float-left"> {{ !empty($data['full_name'])? $data['full_name']: null }}</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="row mt40">
                             <div class="col-sm-6 col-lg-7">
                                 <div class="invoice_date mb70">
-                                    <div class="title">Invoice date:</div>
-                                    <h5 class="fw400 mb0">03/10/2021</h5>
-                                </div>
-                                <div>
-                                    <h3 class="fz22 mb15">Supplier</h3>
-                                    <h5 class="fz16">Guido LLC.</h5>
-                                    <p class="fz15">2301 Ravenswood Rd Madison, <br> WI 53711</p>
+                                    <div class="title">E-poçt</div>
+                                    <h5 class="fw400 mb0">{{ !empty($data['email'])? $data['email']: null }}</h5>
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-5">
                                 <div class="invoice_date mb70">
-                                    <div class="title">Due date:</div>
-                                    <h5 class="fw400 mb0">03/10/2021</h5>
-                                </div>
-                                <div>
-                                    <h3 class="fz22 mb15">Customer</h3>
-                                    <h5 class="fz16">John Doe.</h5>
-                                    <p class="fz15">329 Queensberry Street, North Melbourne VIC <br> 3051, Australia.</p>
+                                    <div class="title">Əlaqə nömrəsi</div>
+                                    <h5 class="fw400 mb0">{{ !empty($data['phone'])? $data['phone']: null }}</h5>
                                 </div>
                             </div>
                         </div>
                         <div class="row mt50">
                             <div class="col-lg-12">
-                                <div class="table-responsive invoice_table_list">
-                                    <table class="table">
-                                        <thead class="thead-light">
-                                        <tr>
-                                            <th class="fw500 color-black22 border-left" scope="col">Description</th>
-                                            <th class="fw500 color-black22" scope="col">Hour</th>
-                                            <th class="fw500 color-black22" scope="col">Quantity</th>
-                                            <th class="fw500 color-black22 border-right" scope="col">Price</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <th class="body-color border-left" scope="row">Design UX and UI</th>
-                                            <td class="body-color">14</td>
-                                            <td class="body-color">3</td>
-                                            <td class="color-black22 border-right">$943</td>
-                                        </tr>
-                                        <tr>
-                                            <th class="body-color border-left" scope="row">Design UX and UI</th>
-                                            <td class="body-color">14</td>
-                                            <td class="body-color">3</td>
-                                            <td class="color-black22 border-right">$943</td>
-                                        </tr>
-                                        <tr>
-                                            <th class="body-color border-left" scope="row">Design UX and UI</th>
-                                            <td class="body-color">14</td>
-                                            <td class="body-color">3</td>
-                                            <td class="color-black22 border-right">$943</td>
-                                        </tr>
-                                        <tr>
-                                            <th scope="row" class="fw500 color-black22 fz18">Total</th>
-                                            <td></td>
-                                            <td></td>
-                                            <td class="fw500 color-black22 fz18">$3,750</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
+                                <div class="invoice_down_print float-right">
+                                    <a class="invoice_print_btn" href="{{ !empty($data['url'])? $data['url']: null }}"> Daxil ol</a>
                                 </div>
                             </div>
                         </div>
@@ -112,9 +62,9 @@
                             <div class="col-lg-8">
                                 <div class="invoice_footer_content">
                                     <ul>
-                                        <li class="list-inline-item"><a href="#">www.guido.com</a></li>
-                                        <li class="list-inline-item"><a href="#">invoice@guido.com</a></li>
-                                        <li class="list-inline-item"><a href="#">(123) 123-456</a></li>
+                                        <li class="list-inline-item"><a href="www.vurtut.com">www.vurtut.com</a></li>
+                                        <li class="list-inline-item"><a href="mailto:info@vurtut.com">info@vurtut.com</a></li>
+                                        <li class="list-inline-item"><a href="tel:+994552956727">+99455 295 67 27</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -124,27 +74,26 @@
             </div>
         </div>
     </section>
-    <a class="scrollToHome" href="#"><i class="fa fa-angle-up"></i></a>
 </div>
 <!-- Wrapper End -->
-<script src="https://vurtut.com/site/js/jquery-3.6.0.js"></script>
-<script src="https://vurtut.com/site/js/jquery-migrate-3.0.0.min.js"></script>
-<script src="https://vurtut.com/site/js/popper.min.js"></script>
-<script src="https://vurtut.com/site/js/bootstrap.min.js"></script>
-<script src="https://vurtut.com/site/js/jquery.mmenu.all.js"></script>
-<script src="https://vurtut.com/site/js/ace-responsive-menu.js"></script>
-<script src="https://vurtut.com/site/js/bootstrap-select.min.js"></script>
-<script src="https://vurtut.com/site/js/snackbar.min.js"></script>
-<script src="https://vurtut.com/site/js/simplebar.js"></script>
-<script src="https://vurtut.com/site/js/parallax.js"></script>
-<script src="https://vurtut.com/site/js/scrollto.js"></script>
-<script src="https://vurtut.com/site/js/jquery-scrolltofixed-min.js"></script>
-<script src="https://vurtut.com/site/js/jquery.counterup.js"></script>
-<script src="https://vurtut.com/site/js/wow.min.js"></script>
-<script src="https://vurtut.com/site/js/progressbar.js"></script>
-<script src="https://vurtut.com/site/js/slider.js"></script>
-<script src="https://vurtut.com/site/js/timepicker.js"></script>
+<script src="{{ asset('site/js/jquery-3.6.0.js') }}"></script>
+<script src="{{ asset('site/js/jquery-migrate-3.0.0.min.js') }}"></script>
+<script src="{{ asset('site/js/popper.min.js') }}"></script>
+<script src="{{ asset('site/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('site/js/jquery.mmenu.all.js') }}"></script>
+<script src="{{ asset('site/js/ace-responsive-menu.js') }}"></script>
+<script src="{{ asset('site/js/bootstrap-select.min.js') }}"></script>
+<script src="{{ asset('site/js/snackbar.min.js') }}"></script>
+<script src="{{ asset('site/js/simplebar.js') }}"></script>
+<script src="{{ asset('site/js/parallax.js') }}"></script>
+<script src="{{ asset('site/js/scrollto.js') }}"></script>
+<script src="{{ asset('site/js/jquery-scrolltofixed-min.js') }}"></script>
+<script src="{{ asset('site/js/jquery.counterup.js') }}"></script>
+<script src="{{ asset('site/js/wow.min.js') }}"></script>
+<script src="{{ asset('site/js/progressbar.js') }}"></script>
+<script src="{{ asset('site/js/slider.js') }}"></script>
+<script src="{{ asset('site/js/timepicker.js') }}"></script>
 <!-- Custom script for all pages -->
-<script src="https://vurtut.com/site/js/script.js"></script>
+<script src="{{ asset('site/js/script.js') }}"></script>
 </body>
 </html>
