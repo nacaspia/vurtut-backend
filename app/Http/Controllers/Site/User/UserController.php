47f7cd50a297921b8cbfb14933fdbc1bf76d2093
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-10 23:29:26 +0400

    test

--- app/Http/Controllers/Site/User/UserController.php
+++ app/Http/Controllers/Site/User/UserController.php
@@ -28,8 +28,8 @@ class UserController extends Controller
     {
         $this->currentLang = Lang::getLocale();
         $this->user = auth('user')->user();
-        dd($this->user);
-         if (!empty($this->user['country_id']) && !empty($this->user['city_id'])) {
+//        dd($this->user);
+         if (!empty($this->user->country_id) && !empty($this->user->city_id)) {
              return self::settings();
          }
     }
