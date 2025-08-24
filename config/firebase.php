a7566cdc1156e67dcab707d6af1bdba701796257
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-24 21:32:05 +0400

    bug

--- config/firebase.php
+++ config/firebase.php
@@ -5,6 +5,13 @@ return [
         'file' => base_path(env('FIREBASE_CREDENTIALS', 'config/firebase_credentials.json')),
     ],
 
-    // Burada yalnız string tipində project adı olmalıdır
+    // Default project ID (string!)
     'default' => env('FIREBASE_PROJECT_ID', 'vurtut'),
+
+    // Lazım gələrsə əlavə layihələr
+    'projects' => [
+        'vurtut' => [
+            'credentials' => base_path(env('FIREBASE_CREDENTIALS', 'config/firebase_credentials.json')),
+        ],
+    ],
 ];
