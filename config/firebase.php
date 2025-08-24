09ee4ebc2442ce976b63a83a293496a981d12303
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-08-25 01:24:04 +0400

    Clean repo (removed secrets)

--- config/firebase.php
+++ config/firebase.php
@@ -1,17 +1,6 @@
 <?php
 return [
-    'credentials' => [
-        'file' => env('FIREBASE_CREDENTIALS'),
-    ],
-
-    'project_id' => env('FIREBASE_PROJECT_ID'),
-
-    'database' => [
-        'url' => env('FIREBASE_DATABASE_URL'),
-    ],
-
-    'storage' => [
-        'default_bucket' => env('FIREBASE_STORAGE_BUCKET'),
-    ],
+    'credentials' => env('FIREBASE_CREDENTIALS'),
+    'database_uri' => env('FIREBASE_DATABASE_URL'),
 ];
 
