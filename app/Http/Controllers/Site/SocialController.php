a056c348ba59cfae284ebf58d650d5dd9e0104c7
Esgerov Enver <enver.esgerov0106@gmail.com>
2025-09-05 13:40:23 +0400

    bug

--- app/Http/Controllers/Site/SocialController.php
+++ app/Http/Controllers/Site/SocialController.php
@@ -19,10 +19,7 @@ class SocialController extends Controller
         }
 
         $redirectUrl = url("social/{$provider}/callback/{$type}");
-
-        // Baxaq Google-a hansı URL göndərilir
-//        dd($redirectUrl);
-
+        dd($redirectUrl);
         return Socialite::driver($provider)->with(['redirect_uri' => $redirectUrl])->redirect();
     }
 
