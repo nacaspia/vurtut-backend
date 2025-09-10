62135924255472d85c095c12ca6b81cc44d5eb81
mammadovailaha <ilamemmedova14@gmail.com>
2025-09-11 02:08:32 +0400

    cards, mobile user profile, footer logo,header logo

--- resources/views/site/user/home.blade.php
+++ resources/views/site/user/home.blade.php
@@ -46,12 +46,12 @@
                         @if(!empty($user['userLikes'][0]))
                             @foreach($user['userLikes'] as $like)
                                 <div class="item">
-                                    <div class="feat_property">
+                                    <div class="feat_property" style="height: 464px;">
                                         <div class="thumb">
                                             <img class="img-whp"  style="width: 100%; max-height: 164px; object-fit: contain; border-radius: 8px; background-color: #f9f9f9;!important;" src="{{ asset("uploads/company/".$like['companyItem']['image']) }}" alt="{{ $like['companyItem']['full_name'] }}">
                                             <div class="thmb_cntnt">
                                                 <ul class="tag mb0">
-                                                    @if($like['companyItem']['is_premium'] == 1)<li class="list-inline-item"><a href="{{ route('site.companyDetails',['slug' => $like['companyItem']['slug']]) }}"> PEMIUM </a></li>@endif
+                                                    @if($like['companyItem']['is_premium'] == 1)<li class="list-inline-item"><a href="{{ route('site.companyDetails',['slug' => $like['companyItem']['slug']]) }}" style="background: radial-gradient(47.12% 309% at 47.12% 40.18%, rgba(254, 255, 134, 0.7) 0%, rgba(251, 206, 61, 0.7) 50.48%, rgba(132, 77, 32, 0.7) 100%); border:none !important;"> Premium </a></li>@endif
                                                 </ul>
                                                 <ul class="listing_reviews">
                                                     <li class="list-inline-item"><a class="text-white" href="{{ route('site.companyDetails',['slug' => $like['companyItem']['slug']]) }}"><span class="fa fa-star"></span></a></li>
@@ -63,20 +63,20 @@
                                                 </ul>
                                             </div>
                                         </div>
-                                        <div class="details">
-                                            <div class="tc_content">
+                                        <div class="details" style="display: flex; flex-direction: column; justify-content: space-between; gap: 20px;">
+                                            <div class="tc_content" style="height: 170px;">
 {{--                                                <div class="badge_icon"><a href="{{ route('site.companyDetails',['slug' => $like['companyItem']['slug']]) }}"><img src="{{ asset("site/images/icons/agent1.svg") }}" alt="agent1.svg"></a></div>--}}
                                                 <h4>{{ $like['companyItem']['full_name'] }}</h4>
                                                 <p>{{ \Illuminate\Support\Str::limit($like['companyItem']['text'], 50, '...') }}</p>
                                                 @php
                                                     $data = $like['companyItem']['data'];
                                                 @endphp
-                                                <ul class="prop_details mb0">
+                                                <ul class="prop_details mb0" style="min-height: 60px; display: flex; flex-direction: column; ">
                                                     <li class="list-inline-item"><a href="tel:{{ $like['companyItem']['phone'] }}"><span class="flaticon-phone pr5"></span> {{ $like['companyItem']['phone'] }}</a></li>
                                                     <li class="list-inline-item"><a href="{{ route('site.companyDetails',['slug' => $like['companyItem']['slug']]) }}"><span class="flaticon-pin pr5"></span>{{ $data['address'] ?? null }}</a></li>
                                                 </ul>
                                             </div>
-                                            <div class="fp_footer">
+                                            <div class="fp_footer" style="width: 100%; display: flex; justify-content: space-between; ">
                                                 <ul class="fp_meta float-left mb0">
                                                     @if(!empty($like['companyItem']['category']['image']))
                                                         <li class="list-inline-item">
